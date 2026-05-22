e4ead7462bc3a82bed5c2650fc8b2e1c21e9632c
kirtymeena <kirtymeena@gmail.com>
2026-05-22 22:12:23 +0530

    created logger middleware

--- server2.js
+++ server2.js
@@ -7,13 +7,33 @@ const users = [
     { id: 4, name: "Jaye" },
 ]
 
+// logger middleware 
+
+/*
+Flow
+
+Client request → Middleware → Route handler → Response
+
+Before route runs, middleware can:
+
+check authentication
+log request
+validate data
+parse JSON
+handle errors
+*/
+const logger = (req, res, next) => {
+    console.log(`${ req.method } ${ req.url }`)
+    next() //moves control to next middleware/route
+}
+
 const server = createServer((req, res) => {
+    logger(req, res, () => { })
     if (req.method === "GET" && req.url === "/api/users") {
         res.setHeader('Content-Type', "application/json")
         res.write(JSON.stringify(users))
         res.end()
-    }
-    else {
+    } else {
         res.setHeader('Content-Type', "application/json")
         res.statusCode = 404
         res.write(JSON.stringify({ message: "Route not found!" }))
