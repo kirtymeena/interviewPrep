a31bf76f7edf31b8904acf16d28a7dca777c9bf6
kirtymeena <kirtymeena@gmail.com>
2026-05-22 21:46:47 +0530

    creating the end point api/users and sending the json to client side as response

---
+++ server2.js
@@ -0,0 +1,21 @@
+import { createServer } from 'http'
+const PORT = process.env.PORT;
+const users = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+    { id: 3, name: "Jim" },
+    { id: 4, name: "Jaye" },
+]
+
+const server = createServer((req, res) => {
+    if (req.method === "GET" && req.url === "/api/users") {
+        res.setHeader('Content-Type', "application/json")
+        res.write(JSON.stringify(users))
+        res.end()
+    }
+})
+
+
+server.listen(PORT, () => {
+    console.log(`server running on port ${ PORT }`)
+})
