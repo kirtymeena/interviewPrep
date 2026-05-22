96b9ac001cc48791b8458e5fbff1a9edcb918dee
kirtymeena <kirtymeena@gmail.com>
2026-05-22 21:31:01 +0530

    added home and about file and accessing them in server.js, and showing the different route's html page in the browser

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 import http from 'http';
+import fs from 'fs/promises'
+import url from 'url'
+import path from 'path';
+
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+// GEt Current path
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log(__filename, __dirname)
+
+
+const server = http.createServer(async (req, res) => {
     try {
         if (req.method === "GET") {
-
+            let filePath;
             if (req.url === "/") {
-                res.writeHead(200, { 'content-type': 'text/html' })
-                res.end(`<h1>Homepage</h1>`)
+                filePath = path.join(__dirname, 'public', 'index.html')
             }
             else if (req.url === "/about") {
-                res.writeHead(200, { 'content-type': 'text/html' })
-                res.end(`<h1>about</h1>`)
+                filePath = path.join(__dirname, 'public', 'about.html')
+
             }
             else {
-                res.writeHead(404, { 'content-type': 'text/html' })
-                res.end(`<h1>not found</h1>`)
+                throw new Error('Not Found')
             }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html')
+            res.write(data)
+            res.end()
         } else {
             throw new Error("method not allowed")
         }
