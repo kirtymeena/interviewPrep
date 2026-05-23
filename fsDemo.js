4274849ae2cf495dde3c96ec2892ee0480eaf24e
kirtymeena <kirtymeena@gmail.com>
2026-05-23 09:16:37 +0530

    promise version and async await version of read file

--- fsDemo.js
+++ fsDemo.js
@@ -1,13 +1,34 @@
-import fs from 'fs'
+// import fs from 'fs'
+import { readFile } from "fs"
+import fs from "fs/promises"
 
 // async version
-fs.readFile('./test.txt', 'utf8', (err, data) => {
-    if (err) {
-        throw err;
-    }
-    console.log(data)
-})
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log(data)
+// })
 
 // synchronous version -  readFileSync()  --> its blocking
-const data = fs.readFileSync("./test.txt", 'Utf8')
-console.log(data)
+// const data = fs.readFileSync("./test.txt", 'Utf8')
+// console.log(data)
+
+
+// readFile() - promise version 
+
+fs.readFile('./test.txt', 'utf8')
+    .then(data => console.log(data)).catch(err => console.log(err))
+
+
+// readFile() - async await - preferred way
+const readFiles = async () => {
+    try {
+        const data = fs.readFile('./test.txt', 'utf8')
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+readFiles()
